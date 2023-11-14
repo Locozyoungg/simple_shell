4c2230cb90f60e2321027c595dd822608ab7b3f0
Locozyoungg <collaustin27@gmail.com>
2023-11-14 15:54:52 -0800

    simplefix

--- simple_shell.c
+++ simple_shell.c
@@ -7,80 +7,83 @@
 
 #define MAX_INPUT_SIZE 1024
 
+/**
+* main - Entry point for the simple shell
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-    char input[MAX_INPUT_SIZE];
+char input[MAX_INPUT_SIZE];
 
-    while (1)
-    {
-        char *token;
-        char *path = "/bin/";
-        char *command;
-        char *args[2];
-        int status;
-        pid_t child_pid;
+while (1)
+{
+char *token;
+char *path = "/bin/";
+char *command;
+char *args[2];
+int status;
+pid_t child_pid;
 
-        printf(":) ");
-        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL)
-        {
-            printf("\n");
-            break;
-        }
+printf(":) ");
+if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL)
+{
+printf("\n");
+break;
+}
 
-        input[strcspn(input, "\n")] = 0;
+input[strcspn(input, "\n")] = 0;
 
-        token = strtok(input, " ");
-        if (token == NULL)
-            continue;
+token = strtok(input, " ");
+if (token == NULL)
+continue;
 
-        command = token;
+command = token;
 
-        if (strcmp(command, "exit") == 0)
-            break;
+if (strcmp(command, "exit") == 0)
+break;
+if (strcmp(command, "env") == 0)
+{
+print_environment();
+continue;
+}
 
-        if (strcmp(command, "env") == 0)
-        {
-            print_environment();
-            continue;
-        }
+token = strtok(NULL, " ");
+args[0] = command;
+args[1] = token;
+args[2] = NULL;
 
-        token = strtok(NULL, " ");
-        args[0] = command;
-        args[1] = token;
-        args[2] = NULL;
+child_pid = fork();
+if (child_pid == -1)
+{
+perror("Fork error");
+exit(EXIT_FAILURE);
+}
 
-        child_pid = fork();
+if (child_pid == 0)
+{
+char *full_command = malloc(strlen(path) + strlen(command) + 1);
+if (full_command == NULL)
+{
+perror("Error");
+exit(EXIT_FAILURE);
+}
+strcpy(full_command, path);
+strcat(full_command, command);
 
-        if (child_pid == -1)
-        {
-            perror("Fork error");
-            exit(EXIT_FAILURE);
-        }
+if (execve(full_command, args, NULL) == -1)
+{
+perror("Error");
+free(full_command);
+exit(EXIT_FAILURE);
+}
+free(full_command);
+}
+else
+{
+wait(&status);
+}
+}
 
-        if (child_pid == 0)
-        {
-            char *full_command = malloc(strlen(path) + strlen(command) + 1);
-            if (full_command == NULL)
-            {
-                perror("Error");
-                exit(EXIT_FAILURE);
-            }
-            strcpy(full_command, path);
-            strcat(full_command, command);
-
-            if (execve(full_command, args, NULL) == -1)
-            {
-                perror("Error");
-                free(full_command);
-                exit(EXIT_FAILURE);
-            }
-            free(full_command);
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
-
-    return (0);
+return (0);
 }
